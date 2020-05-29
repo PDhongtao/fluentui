c889ce92a2d6f96cc4876ba46da5d1f70a2c213e
Oleksandr Fediashov <olfedias@microsoft.com>
2020-05-29 15:13:43 +0200

    fix imports, ignore rules

--- packages/fluentui/react-northstar/src/utils/felaPerformanceEnhancer.ts
+++ packages/fluentui/react-northstar/src/utils/felaPerformanceEnhancer.ts
@@ -1,4 +1,5 @@
 import { ICSSInJSStyle } from '@fluentui/styles';
+// eslint-disable-next-line import/no-extraneous-dependencies
 import cssifyDeclaration from 'css-in-js-utils/lib/cssifyDeclaration';
 import {
   // @ts-ignore
